f21ca06ee70a115758737722f85ffb7f64997267
matuzalempereira <matuzalem_pereiera@hotmail.com>
2022-01-17 00:14:10 -0300

    Corrigir erro de Css no botão

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+const botao = document.querySelector('#botao');
+const imagem1 = document.querySelector('#foto1');
+const nomePersonagem1 = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+const imagem2 = document.querySelector('#foto2');
+const nomePersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+const imagem3 = document.querySelector('#foto3');
+const nomePersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
 
 traduzirCondicao = (data) => {
     if(data.status == 'unknown'){
@@ -19,7 +27,15 @@ gerarValorAletorio = () => {
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAletorio();
+    let numeroAleatorio1 = gerarValorAletorio();
+    let numeroAleatorio2 = gerarValorAletorio();
+    let numeroAleatorio3 = gerarValorAletorio();
+    obterInformacaoPersonagem(numeroAleatorio1, imagem1, nomePersonagem1, especie1, condicao1);
+    obterInformacaoPersonagem(numeroAleatorio2, imagem2, nomePersonagem2, especie2, condicao2);
+    obterInformacaoPersonagem(numeroAleatorio3, imagem3, nomePersonagem3, especie3, condicao3);
+}    
+
+obterInformacaoPersonagem = (numeroAleatorio, imagem, nome, especie, condicao) => {
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers: {
@@ -29,10 +45,11 @@ pegarPersonagem = () => {
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.name;
+        nome.innerHTML = data.name;
         especie.innerHTML = data.species;
         condicao.innerHTML = traduzirCondicao(data);
     });
+    
 }
 
 botao.onclick = pegarPersonagem;
