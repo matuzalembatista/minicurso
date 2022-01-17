f8d279ce2bf71773f2e38ddbaedb64432471c030
matuzalempereira <matuzalem_pereiera@hotmail.com>
2022-01-16 22:33:03 -0300

    Corrigir erro no butão

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('botao');
-const nomeDoPersonagem =  document.querySelector('#nome');
-const especie =  document.querySelector('#especie');
-const condicao =  document.querySelector('#status');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
 
-gerarValorAleatorio = () => {
+gerarValorAletorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
+    let numeroAleatorio = gerarValorAletorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers: {
-            Accept:'application/json',
-            "Content-type": 'application/jason'
+            Accept: 'application/json',
+            "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
         especie.innerHTML = data.species;
-        condicao.innerHTML = data.status;
+        condicao.innerHTML = traduzirCondicao(data);
     });
 }
+
 botao.onclick = pegarPersonagem;
