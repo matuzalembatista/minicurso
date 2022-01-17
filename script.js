2eb2b665d4b9a5814cfbbe557975978c5972497e
matuzalempereira <matuzalem_pereiera@hotmail.com>
2022-01-16 22:07:12 -0300

    Criar consumo de API Rick and Morty

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('botao');
 const nomeDoPersonagem =  document.querySelector('#nome');
 const especie =  document.querySelector('#especie');
-const status =  document.querySelector('#status');
+const condicao =  document.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept:'application/json',
+            "Content-type": 'application/jason'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+botao.onclick = pegarPersonagem;
