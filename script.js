fc51ac2ec7bb699acddcbeb973189b9ddfcea3e7
matuzalempereira <matuzalem_pereiera@hotmail.com>
2022-01-16 22:37:17 -0300

    corrigir resposta da condição Estavivo

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
 gerarValorAletorio = () => {
     return Math.floor(Math.random() * 671);
 }
